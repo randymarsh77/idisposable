3aa8ebd3e04ffcb8d0bc3135b18f664376d418db
Matthew Preucil <mattpsbs@gmail.com>
2019-10-22 21:23:49 -0700

    Update to Swift 5.1

--- Package.swift
+++ Package.swift
@@ -1,5 +1,16 @@
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
-    name: "IDisposable"
+    name: "IDisposable",
+    products: [
+        .library(
+            name: "IDisposable",
+            targets: ["IDisposable"]),
+    ],
+    targets: [
+        .target(
+            name: "IDisposable",
+            dependencies: []),
+    ]
 )
