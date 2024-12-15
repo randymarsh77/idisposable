93137c01e6b4c010de980e31360230e1db1f3415
randymarsh77 <mattpsbs@gmail.com>
2024-12-15 08:59:17 -0800

    Add IAsyncDisposable.

--- Sources/IDisposable/IDisposable.swift
+++ Sources/IDisposable/IDisposable.swift
@@ -2,6 +2,11 @@ public protocol IDisposable {
 	func dispose()
 }
 
+@available(macOS 10.15.0, *)
+public protocol IAsyncDisposable {
+	func dispose() async
+}
+
 public enum ObjectDisposedException: Error {
 	case objectDisposed
 }
